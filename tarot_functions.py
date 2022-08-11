58ab25374065fd365bf34562b40e45b24ded48b6
dylancortez727 <109814714+dylancortez727@users.noreply.github.com>
2022-08-11 11:10:37 -0500

    Add files via upload
    Provided data for Reversed Major Arcana readings, as well as a function for a 5 card cross spread

--- tarot_functions.py
+++ tarot_functions.py
@@ -14,5 +14,12 @@ def single_spread():
     reading = random.choice(CardsList)
     return str(reading)
 
+#defining a 5 card spread, building upon the 3 card basic_spread()
+def cross_spread():
+    core_reading = random.choice(CardsList)
+    potential_reading = random.choice(CardsList)
+    new_reading = "Core: ", core_reading, "Potential: ", potential_reading
+    return basic_spread(), new_reading
+
 #print readout of basic 3 card spread
 print(str(basic_spread()))
