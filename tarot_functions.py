916aaa0f742aeb96e38e5f167843bdee2078ddd0
Dylan <109814714+dylancortez727@users.noreply.github.com>
2022-08-18 20:21:26 -0500

    Implemented minor arcana reading capability, as well as fixes to the drawing functions and random card generator. Fixed some text formatting for better readability

--- tarot_functions.py
+++ tarot_functions.py
@@ -3,30 +3,39 @@ import random
 
 #defining function to return basic 3 card spread for Past, Present, and Future
 def basic_spread():
-    choice1 = random.choice(MajArcana)
-    choice2 = random.choice(MajArcana)
-    choice3 = random.choice(MinArcana)
-    choice4 = random.choice(MinArcana)
-    choice5 = random.choice(MinReversed)
-    choice6 = random.choice(MinReversed)
-    choice_list = [choice1, choice2, choice3, choice4, choice5, choice6]
-    past_reading = random.choice(choice_list)
-    present_reading = random.choice(choice_list)
-    future_reading = random.choice(choice_list)
-    reading = "Past:", past_reading, "Present:", present_reading, "Future:", future_reading
+    lists = MajArcana + MinArcana + MinReversed
+    past_reading = random.choice(lists)
+    present_reading = random.choice(lists)
+    future_reading = random.choice(lists)
+    reading = "Past: {past} \n\nPresent: {present} \n\nFuture: {future}".format(past=past_reading, present=present_reading, future=future_reading)
     return str(reading)
 
 #defining function to return a single-card spread
 def single_spread():
-    reading = random.choice(MajArcana)
+    lists = MajArcana + MinArcana + MinReversed
+    reading = random.choice(lists)
     return str(reading)
 
 #defining a 5 card spread, building upon the 3 card basic_spread()
 def cross_spread():
-    core_reading = random.choice(MajArcana)
-    potential_reading = random.choice(MajArcana)
-    new_reading = "Core: ", core_reading, "Potential: ", potential_reading
-    return basic_spread(), new_reading
+    lists = MajArcana + MinArcana + MinReversed
+    past_reading = random.choice(lists)
+    present_reading = random.choice(lists)
+    future_reading = random.choice(lists)
+    core_reading = random.choice(lists)
+    potential_reading = random.choice(lists)
+    new_reading = "Past: {past}\n\nPresent: {present}\n\nFuture: {future}\n\nCore: {core}\n\nPotential: {potential}".format(past=past_reading, present=present_reading, future=future_reading, core=core_reading, potential=potential_reading)
+    return new_reading
 
-#print readout of basic 3 card spread
-print(str(basic_spread()))
+#user input for what tarot spread they would like
+spread = input("What tarot spread would you like today?")
+
+if spread.upper() == "BASIC SPREAD":
+    print(str(basic_spread()))
+elif spread.upper() == "CROSS SPREAD":
+    print(str(cross_spread()))
+elif spread.upper() == "SINGLE SPREAD":
+    print(str(single_spread()))
+else:
+    print("Not a valid input")
+
